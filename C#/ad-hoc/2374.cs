4d059b21b010410a989d29fe4b53207461185b11
ebs <131674025+enzobernardis@users.noreply.github.com>
2024-02-15 18:43:53 -0300

    Add files via upload

---
+++ C#/ad-hoc/2374.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+  static void Main(string[] args) {
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(a - b);
+  }
+
+}
