d8b36d07dddd3685f0c68c6121ffaaa8fb822594
lumi <131674025+enzobernardis@users.noreply.github.com>
2024-03-07 19:42:12 -0300

    Create 1060.java

---
+++ Java/begginer/1060.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        for(int i = 0; i < 6; i++) {
+            double input = scanner.nextDouble();
+            
+            if(input < 0) count++;
+        }
+        
+        System.out.println(count + " valores positivos");
+    }
+ 
+}
