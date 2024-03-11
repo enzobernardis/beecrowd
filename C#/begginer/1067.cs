67a9001df56f28482cef64b098753be6720a43d4
lumi <131674025+enzobernardis@users.noreply.github.com>
2024-03-10 22:50:37 -0300

    Create 1067.cs

---
+++ C#/begginer/1067.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+  static void Main(string[] args) { 
+    int input = int.Parse(Console.ReadLine());
+    for(int i = 1; i <= input; i+=2) {
+      Console.WriteLine(i);
+    }
+  }
+
+}
