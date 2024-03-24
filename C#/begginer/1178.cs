e49f7a8e6ea1cf0d45665dc07d9ecdc3c3de4887
lumi <131674025+enzobernardis@users.noreply.github.com>
2024-03-24 09:50:47 -0300

    Create 1178.cs

---
+++ C#/begginer/1178.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+
+  static void Main(string[] args) {
+    decimal[] n = new decimal[100];
+    n[0] = decimal.Parse(Console.ReadLine());
+
+    for(int i = 1; i < 100; i++) { 
+      n[i] = n[i - 1] / 2;
+    }
+
+    for(int i = 0; i < 100; i++) Console.WriteLine($"N[{i}] = {Math.Round(n[i], 4):F4}");
+  }
+}
