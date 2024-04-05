60b8a941a1d9d873af97f45f9e624a7662d7140e
lumi <131674025+enzobernardis@users.noreply.github.com>
2024-04-05 11:08:30 -0300

    Create 2581.cs

---
+++ C#/begginer/2581.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int n = int.Parse(Console.ReadLine());
+        
+        for(int i = 0; i < n; i++) Console.ReadLine();
+        for(int i = 0; i < n; i++) Console.WriteLine("I am Toorg!");
+        
+    }
+
+}
