d4b6117d64fcb995897e723e50f9ac123bd7dcb7
lumi <131674025+enzobernardis@users.noreply.github.com>
2024-04-11 21:46:16 -0300

    Create 2764.cs

---
+++ C#/begginer/2764.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        string[] n = Console.ReadLine().Split('/');
+        
+        Console.WriteLine($"{n[1]}/{n[0]}/{n[2]}");
+        Console.WriteLine($"{n[2]}/{n[1]}/{n[0]}");
+        Console.WriteLine($"{n[0]}-{n[1]}-{n[2]}");
+    }
+
+}
