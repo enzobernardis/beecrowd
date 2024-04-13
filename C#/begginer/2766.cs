e8d9b05c5048a6602bda08483072aec156c6b98a
lumi <131674025+enzobernardis@users.noreply.github.com>
2024-04-13 09:10:14 -0300

    Create 2766.cs

---
+++ C#/begginer/2766.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        string[] list = new string[10];
+        
+        for(int i = 0; i < 10; i++) list[i] = Console.ReadLine();
+        
+        Console.WriteLine(list[2]);
+        Console.WriteLine(list[6]);
+        Console.WriteLine(list[8]);
+    }
+
+}
