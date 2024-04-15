080769451f3af47334df4b5b17fe09f4403c94d8
lumi <131674025+enzobernardis@users.noreply.github.com>
2024-04-15 19:29:00 -0300

    Create 2748.cs

---
+++ C#/begginer/2748.cs
@@ -0,0 +1,19 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        string[] lines = {
+            "---------------------------------------",
+            "|        Roberto                      |",
+            "|                                     |",
+            "|        5786                         |",
+            "|                                     |",
+            "|        UNIFEI                       |",
+            "---------------------------------------"
+        };
+        
+        foreach(string line in lines) Console.WriteLine(line);
+    }
+
+}
