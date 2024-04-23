56d4759d47ad7154f3f800d8239bda35b798aa22
lumi <131674025+enzobernardis@users.noreply.github.com>
2024-04-23 14:12:07 -0300

    Create 1067.java

---
+++ Java/begginer/1067.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        for(int i = 1; i <= n; i++) if(i % 2 != 0) System.out.println(i);
+    }
+ 
+}
